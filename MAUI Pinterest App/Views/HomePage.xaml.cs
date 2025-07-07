ed9496eff9f18a4a3dc43c98f0adb42a62e1bfd3
Tashenica Tulley <85678340+Tashenica@users.noreply.github.com>
2025-07-08 00:47:07 +0200

    Added some changes

--- MAUI Pinterest App/Views/HomePage.xaml.cs
+++ MAUI Pinterest App/Views/HomePage.xaml.cs
@@ -13,22 +13,22 @@ public partial class HomePage : ContentPage
         {
             new PinItem { ImageUrl="pin1.png", 
                           Title="Nail Designs", 
-                          Description="This manicure features a square-shaped French tip design on natural pink nude base nails. The look combines bold red French tips with accent nails showcasing a leopard print tip and a fine red smile line, creating a stylish mix of classic elegance and playful animal print art. The nails are finished with a high-gloss top coat, enhancing the sharpness of the red and the detail of the leopard design.This manicure features a square-shaped French tip design on natural pink nude base nails. The look combines bold red French tips with accent nails showcasing a leopard print tip and a fine red smile line, creating a stylish mix of classic elegance and playful animal print art. The nails are finished with a high-gloss top coat, enhancing the sharpness of the red and the detail of the leopard design."},
+                          Description="A set of nude french nails with leopard designs with a tough of red."},
             new PinItem { ImageUrl="pin2.png", 
                           Title="Beauty of Nature", 
-                          Description="This collage beautifully captures warm golden sunsets, peaceful beachscapes, and dreamy silhouettes of nature. It features glowing horizons over calm oceans, silhouetted birds perched on branches, delicate flowers against sunset skies, and serene moments of butterflies resting on blooms. Each image radiates tranquility, natural beauty, and the soothing essence of dusk, creating an overall aesthetic of calm, romance, and reflection." },
+                          Description="A beautiful collage of sunsets." },
             new PinItem { ImageUrl="pin3.png", 
                           Title="Content Ideas", 
-                          Description="This image provides a list of YouTube content ideas on a light purple background with black and pink text. It includes suggestions such as Day In My Life, Mukbang, Night Out With Friends, Skin Care Routine, Clothing Try On Haul, Hair/Nail Appointment, Recent Purchases, Vacation Mini Vlog, Content Creator Essentials, Sunday Reset, Unboxing Video, Cooking/Meal Prep, Sunday Funday, Workout Routine, and Room Makeover. Each idea is accompanied by a heart icon, offering inspiration for lifestyle, beauty, vlog, and daily content creators."},
+                          Description="Some Content Ideas"},
             new PinItem { ImageUrl="pin4.png", 
                           Title="Bible Verse", 
-                          Description="A faith-based motivational image designed as a wallpaper or graphic, featuring the empowering phrase \"Girl, Get Up!\" prominently displayed over a blurred pink heart, along with the corresponding Bible verse Luke 8:54-55, both referenced and quoted in full."},
+                          Description="/Girl, Get Up/ Motivation bible verse"},
             new PinItem { ImageUrl="pin5.png", 
                           Title="Sunset with Daisies", 
-                          Description="The image captures a breathtaking sunset over a vast field filled with white daisies. The sky is painted in brilliant hues of orange, gold, and deep blue, with scattered, textured clouds catching the golden light. The sun is a bright disc just above the distant treeline, casting long rays over the blooming flowers in the foreground, which appear radiant in the warm light."},
+                          Description="A field full of daisies with a amazing sunset."},
             new PinItem { ImageUrl="pin6.png", 
                           Title="Sunflower field", 
-                          Description="A close-up shot of a butterfly with intricate wing patterns, bathed in the warm light of a setting sun. It is positioned among a field of orange and yellow wildflowers, with the glowing sun just above the distant treeline, creating a magical and serene atmosphere."},
+                          Description="A field full of sunflowers with a beautiful sunrise."},
         };
 
         BindingContext = this;
--- MAUI Pinterest App/Views/SearchPage.xaml.cs
+++ MAUI Pinterest App/Views/SearchPage.xaml.cs
@@ -12,6 +12,11 @@ public partial class SearchPage : ContentPage
         Categories = new List<PinItem>
         {
             new PinItem { ImageUrl="category1.png", Title="Date Ideas", Description="A collection of romantic date ideas." },
+            new PinItem { ImageUrl="category2.png", Title="Braiding Styles", Description="Lemonade braids with curls."},
+            new PinItem { ImageUrl="category3.png", Title="Sunflowers", Description="A collage of sunflowers." },
+            new PinItem { ImageUrl="category4.png", Title="Butterfly with Sunflowers", Description="A wallpaper that display a butterfly with sunflowers" },
+            new PinItem { ImageUrl="category5.png", Title="Butterfly in Ocean", Description="A wallpaper with butterflies in the ocean." },
+            new PinItem { ImageUrl="category6.png", Title="Photoshoot Ideas", Description="A perfect spot light photoshoot." },
         };
         BindingContext = this;
     }
