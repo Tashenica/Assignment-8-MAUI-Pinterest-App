bdc0d400f4f21ceee573839799cc76101af5cf5c
Tashenica Tulley <tashenicakleintjie@gmail.com>
2025-07-09 13:47:00 +0200

    Updated code

--- MAUI Pinterest App/Views/SearchPage.xaml.cs
+++ MAUI Pinterest App/Views/SearchPage.xaml.cs
@@ -11,13 +11,14 @@ public partial class SearchPage : ContentPage
 
         Categories = new List<PinItem>
         {
-            new PinItem { ImageUrl="category1.png", Title="Date Ideas", Description="A collection of romantic date ideas.", ImageHeight="250" },
-            new PinItem { ImageUrl="category2.png", Title="Braiding Styles", Description="Lemonade braids with curls.", ImageHeight = "230"},
-            new PinItem { ImageUrl="category3.png", Title="Sunflowers", Description="A collage of sunflowers.", ImageHeight = "250" },
-            new PinItem { ImageUrl="category4.png", Title="Butterfly with Sunflowers", Description="A wallpaper that display a butterfly with sunflowers", ImageHeight = "230"},
-            new PinItem { ImageUrl="category5.png", Title="Butterfly in Ocean", Description="A wallpaper with butterflies in the ocean." , ImageHeight = "250"},
-            new PinItem { ImageUrl="category6.png", Title="Photoshoot Ideas", Description="A perfect spot light photoshoot.", ImageHeight="230" },
+            new PinItem { ImageUrl="category1.png", Title="Date Ideas", Description="A collection of romantic date ideas.", ImageHeight= "" },
+            new PinItem { ImageUrl="category2.png", Title="Braiding Styles", Description="Lemonade braids with curls.", ImageHeight = ""},
+            new PinItem { ImageUrl="category3.png", Title="Sunflowers", Description="A collage of sunflowers.", ImageHeight = "" },
+            new PinItem { ImageUrl="category4.png", Title="Butterfly with Sunflowers", Description="A wallpaper that display a butterfly with sunflowers", ImageHeight = ""},
+            new PinItem { ImageUrl="category5.png", Title="Butterfly in Ocean", Description="A wallpaper with butterflies in the ocean." , ImageHeight = ""},
+            new PinItem { ImageUrl="category6.png", Title="Photoshoot Ideas", Description="A perfect spot light photoshoot.", ImageHeight="" },
         };
         BindingContext = this;
     }
+
 }
